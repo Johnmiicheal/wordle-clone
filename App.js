a2f2d4c53225b8de4c22b3e0dfeb92b6d3382167
Elijah Johnmicheal <michealelijah301@gmail.com>
2022-08-11 00:16:37 +0100

    work in progress

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, ScrollView } from 'react-native';
+import { colors } from './src/constants';
+import Keyboard from './src/components/Keyboard';
+
+const NUMBER_OF_TRIES = 6;
+
+
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split("");
+
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(
+      new Array(letters.length).fill("")
+    ))
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    rows[curRow][curCol] = key;
+    setRows(rows);
+    }
   return (
-    <View style={styles.container}>
-      <Text>WORDLE CLONE!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>WORDLE</Text>
+
+      <ScrollView style={styles.map}>
+        {rows.map(( row, k ) => (
+        <View key={k} style={styles.row}>
+          {row.map((cell, i) => (
+            <View key={i} style={styles.cell}>
+              <Text style={styles.cellText}>
+                {cell.toUpperCase()}
+              </Text>
+            </View>
+          ))}          
+        </View>
+        ))}
+      </ScrollView>
+
+      <Keyboard onKeyPressed={onKeyPressed}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
+    top: 40
+  },
+  map: {
+    marginVertical: 60,
+    alignSelf: "stretch",
+    height: 100,
+  },
+
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+  },
+
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    },
+
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 28,
+  }
 });
--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 From Clone Series presents: WORDLE CLONE mobile 
 
 ## Progress
-* Setting up expo with android emulator
-* Frontend development with javascript/react-native
+* Adding the pressed key to the right cell
+* Saving state, lol
 
 ## Dumb Ideas
 * Add number guessing mode
 * Multiplayer mode
+
+## How its going
+<p align="left">
+    <img src="https://i.imgur.com/DDo9KDF.jpg" alt="wordle_mobile" height= "40px" />
+</p>
